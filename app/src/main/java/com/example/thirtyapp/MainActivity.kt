2e25935cf075a8bfaae3cc0b45b4d99874436f9a
markmacbook <kimji.nattapon@gmail.com>
2020-03-27 22:17:06 +0700

    Update two files. One in the layout. Second in the kotlin.

--- app/src/main/java/com/example/thirtyapp/MainActivity.kt
+++ app/src/main/java/com/example/thirtyapp/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.thirtyapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,5 +13,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // our frist project in Githup
+
+        val text_view = findViewById<EditText>(R.id.text_view)
+        val click_btn = findViewById<Button>(R.id.click_btn)
+        val ImageView = findViewById<ImageView>(R.id.imageView)
+
+
     }
 }
